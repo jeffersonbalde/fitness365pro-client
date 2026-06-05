0a998e2fa36613dc687444b2560f462a34168388
jeffersonbalde <jeffersonbalde8@gmail.com>
2026-06-05 15:29:54 +0800

    as

--- src/utils/apiCache.js
+++ src/utils/apiCache.js
@@ -17,6 +17,9 @@ export const resolveDefaultCacheTtl = (endpoint) => {
   if (DEFAULT_GET_CACHE_TTL_MS[path] != null) {
     return DEFAULT_GET_CACHE_TTL_MS[path]
   }
+  if (path.startsWith('/v1/social/profile/')) {
+    return 30_000
+  }
   if (path.startsWith('/v1/cms/events/') && path.endsWith('/leaderboard')) {
     return 15_000
   }
